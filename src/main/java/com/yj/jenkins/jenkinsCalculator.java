e51a86fa661fa20e2dfe1058dacf452605eb0736
Yj8055 <86706238+Yj8055@users.noreply.github.com>
2021-09-23 15:19:58 +0530

    Update jenkinsCalculator.java

--- src/main/java/com/yj/jenkins/jenkinsCalculator.java
+++ src/main/java/com/yj/jenkins/jenkinsCalculator.java
@@ -1,10 +1,11 @@
 package com.yj.jenkins;
 
 public class jenkinsCalculator {
-	public int addNumbers(int num1,int num2) {
-		return num1+num2;
+	public int addNumbers(int nu1,int num2) {
+		return nu1+num2;
 	}
 	public int subtractNumbers(int num1,int num2) {
 		return num1-num2;
 	}
+	
 }
