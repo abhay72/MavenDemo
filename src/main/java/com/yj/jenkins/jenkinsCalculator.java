1470027dbc72f14dfd914941ea1bcf6a04d849d3
Yj8055 <joshiy8055@gmail.com>
2021-09-23 13:18:07 +0530

    commited maven project to git

---
+++ src/main/java/com/yj/jenkins/jenkinsCalculator.java
@@ -0,0 +1,10 @@
+package com.yj.jenkins;
+
+public class jenkinsCalculator {
+	public int addNumbers(int num1,int num2) {
+		return num1+num2;
+	}
+	public int subtractNumbers(int num1,int num2) {
+		return num1-num2;
+	}
+}
---
+++ src/test/java/com/yj/jenkins/jenkinsCalTest.java
@@ -0,0 +1,18 @@
+package com.yj.jenkins;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class jenkinsCalTest {
+
+	@Test
+	public void addtest() {
+		jenkinsCalculator mycalc=new jenkinsCalculator();
+		assertEquals(10,mycalc.addNumbers(5, 5));
+	}
+	public void subtracttest() {
+		jenkinsCalculator mycalc=new jenkinsCalculator();
+		assertEquals(5,mycalc.subtractNumbers(10, 5));
+	}
+}
