e2c3da64dc88cb892bc7f3491947e603dfd35989
Yj8055 <joshiy8055@gmail.com>
2021-09-25 18:25:01 +0530

    commited for sonar test

--- src/test/java/com/yj/jenkins/jenkinsCalTest.java
+++ src/test/java/com/yj/jenkins/jenkinsCalTest.java
@@ -11,6 +11,7 @@ public class jenkinsCalTest {
 		jenkinsCalculator mycalc=new jenkinsCalculator();
 		assertEquals(10,mycalc.addNumbers(5, 5));
 	}
+	@Test
 	public void subtracttest() {
 		jenkinsCalculator mycalc=new jenkinsCalculator();
 		assertEquals(5,mycalc.subtractNumbers(10, 5));
